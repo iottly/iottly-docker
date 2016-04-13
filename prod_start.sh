78d22878ddd217a0b1972e5c4aadf901c054f47a
Stefano <stefano.terna@tomorrowdata.io>
2016-04-13 05:03:41 +0200

    fixing settings for prod

--- prod_start.sh
+++ prod_start.sh
@@ -2,5 +2,7 @@ export API_HOSTNAME=demoapi.iottly.org
 export BROKER_HOSTNAME=demobroker.iottly.org
 export XMPP_MGMT_REST_SECRET=secret
 export XMPP_PASSWORD=secret
+export XMPP_DOMAIN=xmpphost
+export XMPP_USER=xmppuser
 
 docker-compose -f docker-compose.yml up -d
