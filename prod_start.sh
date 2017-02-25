1427f1cb6d01f170152def688ad86f84fc1a7e09
Stefano <stefano.terna@tomorrowdata.io>
2017-02-25 16:21:36 +0100

    added env vars for core xmpp user, both dev and prod

--- prod_start.sh
+++ prod_start.sh
@@ -3,6 +3,7 @@ export BROKER_HOSTNAME=demobroker.iottly.org
 export XMPP_MGMT_REST_SECRET=secret
 export XMPP_PASSWORD=secret
 export XMPP_DOMAIN=xmpphost
-export XMPP_USER=xmppuser
+export XMPP_USERNAME=xmppuser
+export XMPP_PASSWORD_ENC=enc_secret
 
 docker-compose -f docker-compose.yml up -d
--- start_iottly_locally.sh
+++ start_iottly_locally.sh
@@ -1,4 +1,7 @@
 export HOST_IP_ADDRESS=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
 export XMPP_DOMAIN=xmppbroker.localdev.iottly.org
+export XMPP_USERNAME=iottlycore
+export XMPP_PASSWORD_ENC=0a7581473c8979a9798b339b77817129e6b29fdb87cecd240e16e1ae666d7464
+export XMPP_MGMT_REST_SECRET=EKdj6y0USG4tP4Ki
 
 docker-compose up -d
