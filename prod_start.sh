a9619c6822ffad3d153ae7f989404136607632c2
Stefano <stefano.terna@tomorrowdata.io>
2017-02-28 17:56:13 +0100

    added "maangement" in the format of the topics

--- prod_start.sh
+++ prod_start.sh
@@ -6,5 +6,6 @@ export XMPP_PASSWORD=secret
 export XMPP_DOMAIN=xmpphost
 export XMPP_USERNAME=xmppuser
 export XMPP_PASSWORD_ENC=enc_secret
+export IOTTLY_MQTT_TOPIC_COMMANDS=/iottly/management/+/+/commands
 
 docker-compose -f docker-compose.yml up -d
--- start_iottly_locally.sh
+++ start_iottly_locally.sh
@@ -3,5 +3,6 @@ export XMPP_DOMAIN=xmppbroker.localdev.iottly.org
 export XMPP_USERNAME=iottlycore
 export XMPP_PASSWORD_ENC=0a7581473c8979a9798b339b77817129e6b29fdb87cecd240e16e1ae666d7464
 export XMPP_MGMT_REST_SECRET=EKdj6y0USG4tP4Ki
+export IOTTLY_MQTT_TOPIC_COMMANDS=/iottly/management/+/+/commands
 
 docker-compose up -d
