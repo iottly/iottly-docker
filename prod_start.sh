634b2f1fffaf34e28d97d7fcf5b5c387da63fd96
Stefano <stefano.terna@tomorrowdata.io>
2017-03-09 01:10:13 +0100

    added posthttp ACL management

--- prod_start.sh
+++ prod_start.sh
@@ -1,11 +1,12 @@
 export API_HOSTNAME=demoapi.iottly.org
-export XMPP_BROKER_HOSTNAME}=demobroker.iottly.org
-export MQTT_BROKER_HOSTNAME}=mqttdemobroker.iottly.org
+export XMPP_BROKER_HOSTNAME=demobroker.iottly.org
+export MQTT_BROKER_HOSTNAME=mqttdemobroker.iottly.org
 export XMPP_MGMT_REST_SECRET=secret
 export XMPP_PASSWORD=secret
 export XMPP_DOMAIN=xmpphost
 export XMPP_USERNAME=xmppuser
 export XMPP_PASSWORD_ENC=enc_secret
 export IOTTLY_MQTT_TOPIC_COMMANDS=/iottly/management/+/+/commands
+export IOTTLY_MQTT_TOPIC_EVENTS=/iottly/management/+/+/events
 
 docker-compose -f docker-compose.yml up -d
